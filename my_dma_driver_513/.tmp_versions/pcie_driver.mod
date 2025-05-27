25d1e60922152a4eacfd94b554e22278176a88f5
12wym <108783783+12wym@users.noreply.github.com>
2025-05-27 12:20:24 +0800

    513

---
+++ my_dma_driver_513/.tmp_versions/pcie_driver.mod
@@ -0,0 +1,3 @@
+/home/tronlong/rk3568/rk3/my_dma_driver/pcie_driver.ko
+/home/tronlong/rk3568/rk3/my_dma_driver/pcie_driver.o
+
---
+++ my_dma_driver_513/build.sh
@@ -0,0 +1,2 @@
+export PATH=/home/tronlong/rk3568/rk3/rk356x_linux_release_v1.3.1_20221120/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
+make KDIR=/home/tronlong/rk3568/rk3/rk356x_linux_release_v1.3.1_20221120/kernel CROSS_COMPILE=aarch64-none-linux-gnu-
